b65a0c389e5720ad31e7f7f74d1d719beb910fa7
Justin Tenerelli <jtenerelli1@gmail.com>
2024-04-10 19:35:34 -0400

    finished UI for the most part

--- fovus-react-app/app/page.tsx
+++ fovus-react-app/app/page.tsx
@@ -3,7 +3,10 @@ export default function Home() {
     <div>
       <form>
         <label htmlFor="tinput">Text input: </label>
-        <input type="text" id="tinput" name="tinput"></input>
+        <input type="text" id="tinput" name="tinput"></input><br></br><br></br>
+        <label htmlFor="finput">File input: </label>
+        <input type="file" id="finput" name="finput"></input><br></br><br></br>
+        <input type="submit" value="Submit"></input>
       </form>
     </div>
   );
