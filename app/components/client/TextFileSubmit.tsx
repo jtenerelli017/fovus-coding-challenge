c51485e76ee1406c7f84d81e3c9e7864649651e9
Justin Tenerelli <jtenerelli1@gmail.com>
2024-04-11 18:46:19 -0400

    new feature captures input data

--- app/components/client/TextFileSubmit.tsx
+++ app/components/client/TextFileSubmit.tsx
@@ -1,18 +1,44 @@
-'use client'
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 const TextFileSubmit = () => {
+  const [text, setText] = useState<String>("");
+  const [file, setFile] = useState<File>();
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log(text);
+    if (file != null) {
+      console.log("not null");
+      console.log(file);
+    } else {
+      console.log("null");
+    }
+  };
+
   return (
     <div>
-      <form>
-        <label htmlFor="tinput">Text input: </label>
-        <input type="text" id="tinput" name="tinput"></input><br></br><br></br>
-        <label htmlFor="finput">File input: </label>
-        <input type="file" id="finput" name="finput"></input><br></br><br></br>
-        <input type="submit" value="Submit"></input>
-      </form>
+      <label htmlFor="tinput">Text input: </label>
+      <input type="text" onChange={handleTextChange}></input>
+      <br></br>
+      <br></br>
+      <label htmlFor="finput">File input: </label>
+      <input type="file" onChange={handleFileChange}></input>
+      <br></br>
+      <br></br>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
-  )
-}
+  );
+};
 
-export default TextFileSubmit
+export default TextFileSubmit;
