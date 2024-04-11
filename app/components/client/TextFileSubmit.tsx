6116dca313fe4a0732248b01f2bcfcd39f767903
Justin Tenerelli <jtenerelli1@gmail.com>
2024-04-11 16:53:39 -0400

    combined components for simplicity

---
+++ app/components/client/TextFileSubmit.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+
+const TextFileSubmit = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="tinput">Text input: </label>
+        <input type="text" id="tinput" name="tinput"></input><br></br><br></br>
+        <label htmlFor="finput">File input: </label>
+        <input type="file" id="finput" name="finput"></input><br></br><br></br>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  )
+}
+
+export default TextFileSubmit
